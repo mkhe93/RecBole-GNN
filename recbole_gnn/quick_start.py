fb9eba010d89c424a854d58364244bc85cb22a39
mkhe93 <markus.hoefling@gmx.de>
2024-12-05 11:34:43 +0100

    Added run_recbole_group_gnn.py to allow evaluation on multiple models including GNN models.

--- recbole_gnn/quick_start.py
+++ recbole_gnn/quick_start.py
@@ -8,6 +8,62 @@ from recbole_gnn.utils import create_dataset, data_preparation, get_model, get_t
 from recbole.utils.utils import calculate_valid_score
 
 
+def run(
+    model,
+    dataset,
+    config_file_list=None,
+    config_dict=None,
+    saved=True,
+    nproc=1,
+    world_size=-1,
+    ip="localhost",
+    port="5678",
+    group_offset=0,
+):
+    if nproc == 1 and world_size <= 0:
+        res = run_recbole_gnn(
+            model=model,
+            dataset=dataset,
+            config_file_list=config_file_list,
+            config_dict=config_dict,
+            saved=saved,
+        )
+    else:
+        if world_size == -1:
+            world_size = nproc
+        import torch.multiprocessing as mp
+
+        # Refer to https://discuss.pytorch.org/t/problems-with-torch-multiprocess-spawn-and-simplequeue/69674/2
+        # https://discuss.pytorch.org/t/return-from-mp-spawn/94302/2
+        queue = mp.get_context("spawn").SimpleQueue()
+
+        config_dict = config_dict or {}
+        config_dict.update(
+            {
+                "world_size": world_size,
+                "ip": ip,
+                "port": port,
+                "nproc": nproc,
+                "offset": group_offset,
+            }
+        )
+        kwargs = {
+            "config_dict": config_dict,
+            "queue": queue,
+        }
+
+        mp.spawn(
+            run_recboles,
+            args=(model, dataset, config_file_list, kwargs),
+            nprocs=nproc,
+            join=True,
+        )
+
+        # Normally, there should be only one item in the queue
+        res = None if queue.empty() else queue.get()
+    return res
+
+
 def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict=None, saved=True):
     r""" A fast running api, which includes the complete process of
     training and testing a model on a specified dataset
@@ -19,7 +75,6 @@ def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict
         saved (bool, optional): Whether to save the model. Defaults to ``True``.
     """
     # configurations initialization
-    torch.set_num_threads(24)
     config = Config(model=model, dataset=dataset, config_file_list=config_file_list, config_dict=config_dict)
     try:
         assert config["enable_sparse"] in [True, False, None]
@@ -54,7 +109,7 @@ def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict
 
     # model training
     best_valid_score, best_valid_result = trainer.fit(
-        train_data, valid_data, saved=saved, show_progress=config['show_progress']
+        train_data, test_data, saved=saved, show_progress=config['show_progress']
     )
 
     # model evaluation
@@ -76,7 +131,7 @@ def run_recbole_gnn(model=None, dataset=None, config_file_list=None, config_dict
     }
 
 
-def objective_function(config_dict=None, config_file_list=None, saved=True):
+def objective_function(config_dict=None, config_file_list=None, saved=False):
     r""" The default objective_function used in HyperTuning
 
     Args:
@@ -84,7 +139,6 @@ def objective_function(config_dict=None, config_file_list=None, saved=True):
         config_file_list (list, optional): Config files used to modify experiment parameters. Defaults to ``None``.
         saved (bool, optional): Whether to save the model. Defaults to ``True``.
     """
-
     config = Config(config_dict=config_dict, config_file_list=config_file_list)
     try:
         assert config["enable_sparse"] in [True, False, None]
@@ -97,16 +151,16 @@ def objective_function(config_dict=None, config_file_list=None, saved=True):
     init_seed(config['seed'], config['reproducibility'])
     model = get_model(config['model'])(config, train_data.dataset).to(config['device'])
     trainer = get_trainer(config['MODEL_TYPE'], config['model'])(config, model)
-    best_valid_score, best_valid_result = trainer.fit(train_data, valid_data, verbose=True, saved=saved)
+    best_valid_score, best_valid_result = trainer.fit(train_data, test_data, verbose=True, saved=saved)
     test_result = trainer.evaluate(test_data, load_best_model=saved)
 
     # override valid results with test results
-    best_valid_result = test_result
-    valid_score = calculate_valid_score(test_result, config['valid_metric'])
+    #best_valid_result = test_result
+    #valid_score = calculate_valid_score(test_result, config['valid_metric'])
 
     return {
         'model': config['model'],
-        'best_valid_score': valid_score,
+        'best_valid_score': best_valid_score,
         'valid_score_bigger': config['valid_metric_bigger'],
         'best_valid_result': best_valid_result,
         'test_result': test_result
