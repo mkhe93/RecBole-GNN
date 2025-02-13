d60061bf86bdd29d749edc0091565dd79cfbba1a
mkhe <70406556+mkhe93@users.noreply.github.com>
2025-02-13 10:48:39 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import os
+
+from setuptools import setup, find_packages
+
+install_requires = [
+    "recbole==1.1.1.",
+    "pyg>=2.0.4",
+    "pytorch>=1.7.0"
+]
+
+setup_requires = []
+
+classifiers = ["License :: OSI Approved :: MIT License"]
+
+long_description = (
+    "RecBole-GNN is developed based on Python and PyTorch for "
+    "reproducing and developing recommendation algorithms in "
+    "a unified, comprehensive and efficient framework for "
+    "research purpose. In the first version, our library "
+    "includes 53 recommendation algorithms, covering four "
+    "major categories: General Recommendation, Sequential "
+    "Recommendation, Context-aware Recommendation and "
+    "Knowledge-based Recommendation. View RecBole homepage "
+    "for more information: https://recbole.io"
+)
+
+# Readthedocs requires Sphinx extensions to be specified as part of
+# install_requires in order to build properly.
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+if on_rtd:
+    install_requires.extend(setup_requires)
+
+setup(
+    name="recbole-gnn",
+    version="1.0.0",  # please remember to edit recbole/__init__.py in response, once updating the version
+    description="A unified, comprehensive and efficient recommendation library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkhe93/RecBole-GNN/tree/feature/forward_gnn",
+    author="RecBoleTeam",
+    author_email="recbole@outlook.com",
+    packages=[package for package in find_packages() if package.startswith("recbole")],
+    include_package_data=True,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    extras_require=extras_require,
+    zip_safe=False,
+    classifiers=classifiers,
+)
