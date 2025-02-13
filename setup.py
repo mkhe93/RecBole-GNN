c5ecb4a200a9f688f5d74d94dce16916f4ed3381
mkhe <70406556+mkhe93@users.noreply.github.com>
2025-02-13 15:08:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 from setuptools import setup, find_packages
 
 install_requires = [
-    "git+https://github.com/mkhe93/RecBole.git@/mkhe/thesis,
+    "git+https://github.com/mkhe93/RecBole.git@/mkhe/thesis",
     "torch-geometric>=2.0.4",
     "torch>=1.7.0"
 ]
