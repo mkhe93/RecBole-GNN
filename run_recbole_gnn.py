39a3ff270e0417d5433f322f95d2589046cbeeae
Markus Höfling (DevBoost) <markus.hoefling@devboost.com>
2025-02-14 10:11:33 +0100

    removed forwardgnn approach

--- run_recbole_gnn.py
+++ run_recbole_gnn.py
@@ -1,11 +1,13 @@
 import argparse
-import os
 import torch
 
+import os
+
+import threadpoolctl
 from recbole_gnn.quick_start import run_recbole_gnn
 
 if __name__ == '__main__':
-    model = 'NGCF'
+    model = 'ALS'
     dataset = 'real-life-atomic-100000'
     dataset = 'ml-100k'
 
@@ -17,7 +19,7 @@ if __name__ == '__main__':
     layer_aggregation = 'mean'
     final_aggregation = 'mean'
 
-    torch.set_num_threads(8)
+    #torch.set_num_threads(8)
 
     args, _ = parser.parse_known_args()
 
@@ -29,30 +31,3 @@ if __name__ == '__main__':
         }
         config_file_list = args.config_files.strip().split(' ') if args.config_files else None
         run_recbole_gnn(model=args.model, dataset=args.dataset, config_file_list=config_file_list, config_dict=config_dict, saved=False)
-
-    else:
-        pre_train_epochs = 5
-        save_step = 5
-
-        config_dict = {
-            'train_stage': 'pretrain',
-            'layer_aggregation': layer_aggregation,
-            'final_aggregation': final_aggregation,
-            'pretrain_epochs': pre_train_epochs,
-            'save_step': save_step
-        }
-        config_file_list = args.config_files.strip().split(' ') if args.config_files else None
-        run_recbole_gnn(model=args.model, dataset=args.dataset, config_file_list=config_file_list,
-                        config_dict=config_dict, saved=False)
-
-        config_dict = {
-            'train_stage': 'finetune',
-            'epochs': 5,
-            'pre_model_path': f'./saved/{model}-{dataset}-{pre_train_epochs}.pth'
-        }
-        run_recbole_gnn(model=args.model, dataset=args.dataset, config_file_list=config_file_list,
-                        config_dict=config_dict)
-
-## 16 Jan 08:13
-# 'precision@10': 0.0284, 'hit@10': 0.2225, 'mrr@10': 0.0948, 'ndcg@10': 0.0608,
-# 'map@10': 0.0332, 'itemcoverage@10': 0.1451, 'averagepopularity@10': 44.1465, 'tailpercentage@10': 0.0037})
